554ee9b7a96f1e65cdf2a65b1cb4ce820e264a96
cyy <shawn199101@163.com>
2016-06-23 18:08:30 +0800

    Initial commit

--- app/src/main/java/com/cyy/imagebrowserdemo/MainActivity.java
+++ app/src/main/java/com/cyy/imagebrowserdemo/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 
-
 public class MainActivity extends AppCompatActivity {
 
     protected RecyclerView recycleView;
